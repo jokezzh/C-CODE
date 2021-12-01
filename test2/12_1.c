db4e465e94af9370a717ba019fac0861c6f64117
jokezzh <xzh.0804@qq.com>
2021-12-01 13:52:47 +0800

    12_1学习

---
+++ test2/12_1.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码:"); 
+//	scanf("%s", password);
+//	printf("请确认密码 (Y/N):"); 
+//	getchar();
+//	int ch = getchar();
+//
+//	if (ch == 'Y')
+//	{
+//		printf("确认成功");
+//	}
+//	else
+//	{
+//
+//		printf("确认失败");
+//	}
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i); 
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i] );
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 3; j++)
+//			printf("hehe\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 3; i++)
+//    {
+//		for (; j < 3; j++)
+//			printf("hehe\n");
+//	}
+//	return 0;
+//}
+int main()
+{
+	int x, y;
+	for (x = 0, y = 0; x < 2 && y < 5; ++x, y++)
+	{
+		printf("hehe\n");
+	}
+
+	return 0;
+}
