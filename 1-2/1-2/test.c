43c42516dd1413f7841862914717aba861b30d30
jokezzh <xzh.0804@qq.com>
2022-01-21 17:23:54 +0800

    函数学习

---
+++ 1-2/1-2/test.c
@@ -0,0 +1,116 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int my_strlen(char*str)
+//{
+//	int count = 0;
+//	while(*str!='\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+// }
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+// int main()
+//{
+//	char arr[] = "bit";
+//	printf("%d\n", my_strlen(arr));
+//	return 0;
+//}
+//int num(int n)
+//{
+//	if (n > 1)
+//		return n * num(n - 1);
+//	else
+//		return 1;
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d\n", num(n));
+//	return 0;
+//}
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n>2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = Fib(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//		{
+//			count++;
+//		}
+//		if (i / 10 == 9)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 0)
+//			sum -= 1.0 / i;
+//		else
+//			sum += 1.0 / i;
+//	}
+//	printf("%lf\n", sum);
+//
+//	return 0;
+//}
+ int main()
+{
+	int i = 0;
+	double sum = 0.0;
+	int flag = 1;
+	for (i = 1; i <= 100; i++)
+	{
+		sum += flag * 1.0 / i;
+		flag = -flag;
+	}
+	printf("%lf\n", sum);
+
+	return 0;
+}
