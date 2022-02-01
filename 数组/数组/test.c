7614c2f2731439c5cd410b2300cb694fc2ab0e1a
jokezzh <xzh.0804@qq.com>
2022-02-01 20:57:24 +0800

    数组

---
+++ 数组/数组/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int arr[3][4] = { {1,2},{3,4},{5,6} };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+	/*int main()
+	{
+	int arr[3][4] = { {1,2},{3,4},{5,6} };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]=%p ",i,j, &arr[i][j]);
+		}
+		
+	}
+	return 0;
+}*/
+//void b_d(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < sz-1-i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] =  tmp;
+//
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	b_d(arr, sz);
+//	return 0;
+//}
+
